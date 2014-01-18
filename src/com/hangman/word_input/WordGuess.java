dae18a640b9d9924e63e966c01223c7353135d8d
mb05032 <mb05032@users.sourceforge.net>
2014-01-18 18:04:06 -0500

    Added WordGuess Activity

---
+++ src/com/hangman/word_input/WordGuess.java
@@ -0,0 +1,22 @@
+package com.hangman.word_input;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class WordGuess extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_word_guess);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.word_guess, menu);
+		return true;
+	}
+
+}
--- src/com/hangman/word_input/WordInput.java
+++ src/com/hangman/word_input/WordInput.java
@@ -1,6 +1,7 @@
 package com.hangman.word_input;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -28,7 +29,8 @@ public class WordInput extends Activity
         else {
             word = temp_word;
             Toast.makeText(getApplicationContext(), (CharSequence)word, Toast.LENGTH_SHORT).show();
-            // TODO: show the guessing activity
+            Intent word_guess = new Intent(getApplicationContext(), com.hangman.word_input.WordGuess.class);
+            startActivity(word_guess);
         }
     }
 
