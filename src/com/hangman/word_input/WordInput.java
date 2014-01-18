4b2e243a53ff8ad919cb819886015df6a7a724d6
mb05032 <mb05032@users.sourceforge.net>
2014-01-18 17:34:41 -0500

    Added word input activity

--- src/com/hangman/word_input/WordInput.java
+++ src/com/hangman/word_input/WordInput.java
@@ -2,6 +2,9 @@ package com.hangman.word_input;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class WordInput extends Activity
 {
@@ -12,4 +15,22 @@ public class WordInput extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+    // Called when the OK button is clicked
+    // Checks to make sure the input is only alphabetic characters
+    public void click_ok(View view) {
+        EditText input = (EditText)findViewById(R.id.input);
+        String temp_word = input.getText().toString().toLowerCase();
+        if ( !temp_word.matches("^[a-z]+$") ) {
+            Toast.makeText(getApplicationContext(), "Please enter a valid word", Toast.LENGTH_SHORT).show();
+        }
+        // TODO: Enforce other constraints on the input (length?)
+        else {
+            word = temp_word;
+            Toast.makeText(getApplicationContext(), (CharSequence)word, Toast.LENGTH_SHORT).show();
+            // TODO: show the guessing activity
+        }
+    }
+
+    private String word;
 }
