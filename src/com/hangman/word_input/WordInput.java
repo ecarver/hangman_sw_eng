00f65d305643d826e97a62605ef20540fe27de09
mb05032 <mb05032@users.sourceforge.net>
2014-01-18 15:54:34 -0500

    Added hangman project

---
+++ src/com/hangman/word_input/WordInput.java
@@ -0,0 +1,15 @@
+package com.hangman.word_input;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WordInput extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
